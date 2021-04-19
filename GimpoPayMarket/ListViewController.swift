35d3430ca80857e048df0b66803129fa091fc240
hhhan0315 <hhan0315@naver.com>
2021-04-19 21:49:04 +0900

    스크롤뷰 가맹점 종류 선택 스크롤뷰와 스택뷰 활용 버튼 style 설정

--- GimpoPayMarket/ListViewController.swift
+++ GimpoPayMarket/ListViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    private var buttons = [UIButton]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setNavigationTitle()
+        setStackViewInScrollView()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
     func setNavigationTitle() {
@@ -31,6 +39,49 @@ class ListViewController: UIViewController {
         
         self.navigationItem.titleView = label
     }
+    
+    func setStackViewInScrollView() {
+        // storyboard 오류 수정을 위해 button 임의로 추가해둔 것 삭제
+        removeAllSubViews()
+        
+        let titles = ["내 주변", "병원/약국", "슈퍼/마트", "스포츠/헬스", "미용/뷰티/위생", "레저", "학원/교육", "부동산/인테리어", "숙박/캠핑", "도서/문화/공연", "시장/거리", "전통시장/상점가", "일반음식점", "분식", "카페/베이커리", "산모/육아", "의류/잡화/안경", "자동차/자전거", "주유소", "가전/통신", "로컬친환경", "기타"]
+        var buttonTag = 0
+        
+        for title in titles {
+            let button = UIButton()
+            button.setTitle(title, for: .normal)
+            button.setTitleColor(.black, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+            button.layer.borderWidth = 1
+            button.layer.cornerRadius = 15
+            button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+            button.addTarget(self, action: #selector(touchUpButton(_:)), for: .touchUpInside)
+            button.tag = buttonTag
+            buttonTag += 1
+            
+            buttons.append(button)
+            stackView.addArrangedSubview(button)
+        }
+    }
+    
+    func removeAllSubViews() {
+        for subview in stackView.arrangedSubviews {
+            subview.removeFromSuperview()
+        }
+    }
+    
+    @objc func touchUpButton(_ sender:UIButton) {
+        for button in buttons {
+            if button.tag == sender.tag {
+                button.backgroundColor = .gray
+                button.setTitleColor(.white, for: .normal)
+            } else {
+                button.backgroundColor = .none
+                button.setTitleColor(.black, for: .normal)
+            }
+        }
+    }
+    
 
 }
 
